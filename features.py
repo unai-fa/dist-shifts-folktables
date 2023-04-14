9e4a757e58f3d1123a90123646f2e12fddafd43a
Unai Fischer Abaigar <unai.fischerabaigar@icloud.com>
2023-04-14 11:21:37 +0200

    Initial commit

---
+++ features.py
@@ -0,0 +1,5 @@
+
+FEATURE_SETS = {
+    'WORK': ['COW', 'OCCP', 'WKHP', 'ESP', 'MIG', 'ESR', 'FER', 'JWMNP', 'JWTR', 'POWPUMA'],
+    'SENSITIVE': ['RELP', 'SEX', 'RAC1P', 'DIS', 'CIT', 'ANC', 'DEAR', 'DEYE', 'DREM', 'GCL']
+} 
